42941d0ffbd0d24b5718269224aa9ab846ff9b8f
krishna-windows <krishna444@gmail.com>
2018-05-15 21:00:00 +0200

    methods validation

--- StackNQueues/MyQueue.js
+++ StackNQueues/MyQueue.js
@@ -7,22 +7,42 @@ class MyQueue {
         this.limit = limit;
         this.head = 0;
         this.tail = 0;
+        this.size = 0;
         this.A = new Array(limit);
     }
 
     enqueue(element) {
-        this.A[this.tail++] = element;
-
+        this.A[this.tail] = element;
+        this.tail = (this.tail + 1) % this.limit;
+        this.size++;
     }
 
     dequeue() {
-        let value = this.A[this.head++];
+        if (this.isEmpty()) {
+            console.log("Empty!");
+            return;
+        }
+        let value = this.A[this.head];
+        this.head = (this.head + 1) % this.limit;
+        this.size--;
         console.log("Dequeue:" + value);
         return value;
     }
 
     peek() {
+        if (this.isEmpty()) {
+            console.log("Empty!")
+            return;
+        }
         return this.A[this.head];
+    }
+
+    isEmpty() {
+        return this.size == 0 ? true : false;
+    }
+
+    toString() {
+        return this.A.toString();
     }
 }
 
@@ -30,9 +50,4 @@ let queue = new MyQueue(100);
 queue.enqueue(19);
 queue.enqueue(39);
 queue.enqueue(49);
-console.log(queue.peek());
-console.log(queue.tail)
-
-queue.dequeue();
-queue.dequeue();
-console.log(queue.peek());
+console.log(queue.toString());
